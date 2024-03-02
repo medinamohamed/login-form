41db1fb1d428915f57271dd2ada2e04d79c0ff9c
Medina <medina.ahmed.mo@gmail.com>
2024-03-02 16:22:09 -0500

    login schema

--- src/mongodb.js
+++ src/mongodb.js
@@ -8,17 +8,17 @@
 //     console.log("failed to connect")
 // })
 
-// const LogInSchema = new mongoose.Schema({
-//     name:{
-//         type:String,
-//         required:true
-//     },
-//     password:{
-//         type:String,
-//         required:true
-//     }
-// })
+const LogInSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
 
-// const collection = new mongoose.model("Collection1", LogInSchema)
+const collection = new mongoose.model("Collection1", LogInSchema)
 
-// module.exports= collection
+module.exports= collection
